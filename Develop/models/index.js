1e8ef7dc00de175da8b3d65fb6b355ac3b0d9d5b
joe <jajohnson0201@gmail.com>
2023-03-30 19:35:35 -0500

    got models and cat routes done

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsToMany(Category)
 // Categories have many Products
-
+Category.hasMany(Product)
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  though: {
+    model: ProductTag,
+    unique: false
+  },
+  as: "product-tags"
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  though: {
+    model: ProductTag,
+    unique: false
+  },
+  as: "tag-products"
+});
 
 module.exports = {
   Product,
